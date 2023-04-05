e9907b7e36be2c022e7b788aaac74efc26533cca
gimgihyeon <gihyeon6394@gmail.com>
2023-04-05 18:18:34 +0900

    feat : add Item1.java

--- src/main/java/com/effectiveJava/chapter2/item1/Item1.java
+++ src/main/java/com/effectiveJava/chapter2/item1/Item1.java
@@ -3,6 +3,6 @@ package com.effectiveJava.chapter2.item1;
 public class Item1 {
 
     public static void main(String[] args){
-        System.out.println("Hello Item 1!");
+        System.out.println("Hello Item !");
     }
 }

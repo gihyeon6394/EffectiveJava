a152734aee9e7ee36c2d09bc11c39fa7130cf77a
gihyeon <gihyeon6394@gmail.com>
2023-05-01 14:18:02 +0900

    feat : end Item 16

--- src/main/java/com/effectiveJava/chapter4/item16/Item16.java
+++ src/main/java/com/effectiveJava/chapter4/item16/Item16.java
@@ -1,7 +1,72 @@
 package com.effectiveJava.chapter4.item16;
 
 /**
-* item16. public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라
-* */
+ * item16. public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라
+ */
 public class Item16 {
+
+    public static void main(String[] args) {
+
+        Foo foo = new Foo();
+        foo.a = 1;
+    }
+
+    class Point1 {
+        public double x;
+        public double y;
+    }
+
+    class Point2 {
+        private double x;
+        private double y;
+
+        public Point2(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+    }
+
+    /**
+     * 불편 필드를 public으로 노출한 클래스
+     */
+
+    final class Time {
+        private static final int HOURS_PER_DAY = 24;
+        private static final int MINUTES_PER_HOUR = 60;
+
+        public final int hour;
+        public final int minute;
+
+        public Time(int hour, int minute) {
+            if (hour < 0 || hour >= HOURS_PER_DAY) {
+                throw new IllegalArgumentException("시간: " + hour);
+            }
+            if (minute < 0 || minute >= MINUTES_PER_HOUR) {
+                throw new IllegalArgumentException("분: " + minute);
+            }
+            this.hour = hour;
+            this.minute = minute;
+        }
+    }
+
+    private static class Foo {
+        public int a;
+        public int b;
+    }
 }
--- src/main/java/com/effectiveJava/chapter4/item16/README.md
+++ src/main/java/com/effectiveJava/chapter4/item16/README.md
@@ -1,4 +1,32 @@
 <h1>item 16. public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라</h1>
 
-> 
+> 가변필드라면 public으로 직접 노출하지 마라. 불변필드면 상관없지만 여전히 불편함이 있음
+> 그러니까 public 클래스의 필드는 public이 아니어야 한다
 
+~~~~
+class Point{
+    public double x;
+    public double y;
+}
+~~~~
+
+- 내부 표현 수정 시 API를 수정해야함
+- 불변식을 보장할 수 없음
+- 외부에서 필드에 접근할 때 부수 작업을 수행할 수 없음
+
+<h2>따라서 public class의 필드를 private으로 낮추고 접근자 메서드를 사용</h2>
+
+- package-private, private 중첩 클래스라면 필드를 public으로 두어도 문제 없음
+    - 접근자 방식보다 클라이언트 코드도 더 깔끔함
+    ~~~~
+    private static class Foo{
+        public int a;
+        public int b;
+    }
+    ...
+    Foo foo = new Foo();
+    foo.a = 1;
+    ~~~~
+- 필드가 **불변**이라면 public으로 두어도 되지만...
+    - API를 변경하지 않고는 표현 방식을 바꿀 수 없고
+    - 필드를 읽을 때 부수 작업을 수행할 수 없음
